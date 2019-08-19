dbcbbc24118d73c0d9bfedabb6951b799bb48171
Svetlin Stoyanov <svetlinst@gmail.com>
2019-08-19 23:20:59 +0300

    Add project files.

---
+++ L01_SortByNameAndAge/Person.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (value.Length<3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                this.lastName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value <=0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if (value<460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                this.salary = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{firstName} {lastName} gets {salary:f2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.age>30)
+            {
+                this.salary += this.salary * percentage / 100;
+            }
+            else
+            {
+                this.salary += this.salary * percentage / 200;
+            }
+
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary) : this(firstName, lastName, age)
+        {
+            this.Salary = salary;
+        }
+    }
+}
---
+++ L01_SortByNameAndAge/StartUp.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            Team team = new Team("Test");
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+
+                try
+                {
+                    var person = new Person(cmdArgs[0],
+                        cmdArgs[1],
+                        int.Parse(cmdArgs[2]),
+                        decimal.Parse(cmdArgs[3]));
+                    persons.Add(person);
+                    team.AddPlayer(person);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                }
+            }
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+
+            //var parcentage = decimal.Parse(Console.ReadLine());
+            //persons.ForEach(p => p.IncreaseSalary(parcentage));
+            //persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
