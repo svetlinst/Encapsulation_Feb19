853779b1f49c0b1f547ee2850ba95171287ea70c
Svetlin Stoyanov <svetlinst@gmail.com>
2019-08-19 23:55:28 +0300

    P1 added

---
+++ P01_ClassBox/Box.cs
@@ -0,0 +1,74 @@
+﻿namespace P01_ClassBox
+{
+    using System;
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public void GetSurfaceArea()
+        {
+            var area = 2 * length * width + 2 * length * height + 2 * width * height;
+            Console.WriteLine($"Surface Area - {area:f2}");
+        }
+
+        public void GetLateralSurfaceArea()
+        {
+            var area = 2 * this.length * this.height + 2 * this.width * this.height;
+            Console.WriteLine($"Lateral Surface Area - {area:f2}");
+        }
+
+        public void GetVolume()
+        {
+            var volume = length * width * height;
+            Console.WriteLine($"Volume - {volume:f2}");
+        }
+    }
+}
---
+++ P01_ClassBox/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace P01_ClassBox
+{
+    using System;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+                box.GetSurfaceArea();
+                box.GetLateralSurfaceArea();
+                box.GetVolume();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+        }
+    }
+}
