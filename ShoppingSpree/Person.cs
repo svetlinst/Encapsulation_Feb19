361375302d7e7d41533e5ba9685d1852dbe4af02
Svetlin Stoyanov <svetlinst@gmail.com>
2019-08-20 13:41:10 +0300

    P4

---
+++ ShoppingSpree/Person.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bagOfProducts;
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get => this.money;
+            private set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException($"Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            bagOfProducts = new List<Product>();
+        }
+
+        public void BuyProduct(Product product)
+        {
+            if (CanAffordProduct(product))
+            {
+                this.money -= product.Cost;
+                bagOfProducts.Add(product);
+                Console.WriteLine($"{this.Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} can't afford {product.Name}");
+            }
+        }
+
+        private bool CanAffordProduct(Product product)
+        {
+            if (product.Cost<=this.Money)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string GetProducts()
+        {
+            StringBuilder sb = new StringBuilder($"{this.Name} - ");
+            if (bagOfProducts.Count == 0)
+            {
+                sb.Append("Nothing bought");
+            }
+            else
+            {
+                sb.Append(string.Join(", ", bagOfProducts));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ ShoppingSpree/StartUp.cs
@@ -0,0 +1,67 @@
+﻿namespace ShoppingSpree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+            List<Person> persons = new List<Person>();
+            List<Product> products = new List<Product>();
+            foreach (var line in input)
+            {
+                var tokens = line.Split('=', StringSplitOptions.RemoveEmptyEntries);
+                var name = tokens[0];
+                var money = decimal.Parse(tokens[1]);
+                try
+                {
+                    Person curPerson = new Person(name, money);
+                    persons.Add(curPerson);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                    Environment.Exit(0);
+                }
+            }
+
+            input = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in input)
+            {
+                var tokens = line.Split('=', StringSplitOptions.RemoveEmptyEntries);
+                var name = tokens[0];
+                var cost = decimal.Parse(tokens[1]);
+                try
+                {
+                    Product curProduct = new Product(name, cost);
+                    products.Add(curProduct);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                    throw;
+                }
+            }
+
+            string commands;
+            while ((commands = Console.ReadLine())!= "END")
+            {
+                var tokens = commands.Split();
+                var personName = tokens[0];
+                var productName = tokens[1];
+                persons.Where(n => n.Name == personName)
+                    .FirstOrDefault()
+                    .BuyProduct(products.Where(n => n.Name == productName).FirstOrDefault());
+            }
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.GetProducts());
+            }
+        }
+
+    }
+}
