f7b0cebd3ea08a9399a1a14e24ba1ff274cb276f
Svetlin Stoyanov <svetlinst@gmail.com>
2019-08-21 23:58:44 +0300

    New project added but not solved

---
+++ FootballTeamGenerator/Model/Player.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballTeamGenerator.Model
+{
+    public class Player
+    {
+        private string name;
+        private int endurance;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+
+        private const int minSkill = 0;
+        private const int maxSkill = 100;
+
+        public Player(string name, int endurance, int spirit, int dribble, int passing, int shooting)
+        {
+            this.Name = name;
+            this.Endurance = endurance;
+            this.Sprint = sprint;
+            this.Dribble = dribble;
+            this.Passing = passing;
+            this.Shooting = shooting;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                name = value;
+            }
+        }
+
+        public int Endurance
+        {
+            get => this.endurance;
+            private set
+            {
+                if (value<minSkill || value>maxSkill)
+                {
+                    throw new ArgumentException($"{nameof(Endurance)} should be between 0 and 100.");
+                }
+                endurance = value;
+            }
+        }
+
+        public int Sprint
+        {
+            get => this.sprint;
+            private set
+            {
+                if (value < minSkill || value > maxSkill)
+                {
+                    throw new ArgumentException($"{nameof(Sprint)} should be between 0 and 100.");
+                }
+                sprint = value;
+            }
+        }
+
+        public int Dribble
+        {
+            get => this.dribble;
+            private set
+            {
+                if (value < minSkill || value > maxSkill)
+                {
+                    throw new ArgumentException($"{nameof(Dribble)} should be between 0 and 100.");
+                }
+                dribble = value;
+            }
+        }
+
+        public int Passing
+        {
+            get => this.passing;
+            private set
+            {
+                if (value < minSkill || value > maxSkill)
+                {
+                    throw new ArgumentException($"{nameof(Passing)} should be between 0 and 100.");
+                }
+                passing = value;
+            }
+        }
+
+        public int Shooting
+        {
+            get => this.shooting;
+            private set
+            {
+                if (value < minSkill || value > maxSkill)
+                {
+                    throw new ArgumentException($"{nameof(Shooting)} should be between 0 and 100.");
+                }
+                shooting = value;
+            }
+        }
+
+        public double OverallSkillLevel
+        {
+            get
+            {
+                return Passing * Shooting * Dribble * Sprint * Endurance / 5;
+            }
+        }
+    }
+}
---
+++ FootballTeamGenerator/Model/Team.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FootballTeamGenerator.Model
+{
+    public class Team
+    {
+        private string name;
+        private List<Player> players;
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"A name should not be empty.");
+                }
+                name = value;
+            }
+        }
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.players = new List<Player>();
+        }
+
+        public double Rating
+        {
+            get
+            {
+                return this.players.Average(s => s.OverallSkillLevel);
+            }
+        }
+
+        public void AddPlayer(Player player)
+        {
+            this.players.Add(player);
+        }
+
+        public void RemovePlayer(Player player)
+        {
+            this.players.Remove(player);
+        }
+    }
+}
